b74d9b57083ac578c8cc74b45c01232fb67d4558
Surya Subramanian <73920354+codingwithsurya@users.noreply.github.com>
2025-03-19 14:22:32 -0700

    Time: 0 ms (100.00%) | Memory: 17.8 MB (90.29%) - LeetSync

--- 362-design-hit-counter/design-hit-counter.py
+++ 362-design-hit-counter/design-hit-counter.py
@@ -7,14 +7,11 @@ class HitCounter:
     def hit(self, timestamp: int) -> None:
         self.queue.append(timestamp)
         
-    
 
     def getHits(self, timestamp: int) -> int:
         while self.queue and timestamp - self.queue[0] >= 300:
-            self.queue.popleft()
+            self.queue.popleft() 
         return len(self.queue)
-
-
         
 
 
