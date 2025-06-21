9d2c1ae0f0d87b8c883e4a8c8375cee7dee7b70b
Surya Subramanian <73920354+codingwithsurya@users.noreply.github.com>
2025-06-21 14:02:29 -0700

    Time: 11 ms (27.91%) | Memory: 22.1 MB (43.82%) - LeetSync

--- 229-majority-element-ii/majority-element-ii.py
+++ 229-majority-element-ii/majority-element-ii.py
@@ -1,13 +1,15 @@
 class Solution:
     def majorityElement(self, nums: List[int]) -> List[int]:
-        cutoff = len(nums) // 3
-
-        num_to_freq = Counter(nums) 
-        
+        numToCount = defaultdict(int)
+        n = len(nums)
         res = []
-        for num, freq in num_to_freq.items():
-            if freq > cutoff:
+
+        for num in nums:
+            numToCount[num] += 1
+
+        for num, count in numToCount.items():
+            if count > n // 3:
                 res.append(num)
         
-        return res
+        return res 
         
