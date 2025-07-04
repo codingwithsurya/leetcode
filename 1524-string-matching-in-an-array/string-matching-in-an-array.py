3f05e0ebc86f62de3070f7c6e2a6ffc22b303bad
Surya Subramanian <73920354+codingwithsurya@users.noreply.github.com>
2025-07-04 12:07:33 -0700

    Time: 3 ms (66.78%) | Memory: 17.7 MB (84.29%) - LeetSync

--- 1524-string-matching-in-an-array/string-matching-in-an-array.py
+++ 1524-string-matching-in-an-array/string-matching-in-an-array.py
@@ -3,7 +3,7 @@ class Solution:
         res = []
         for w1 in words:
             for w2 in words:
-                if w1 != w2 and (w1 in w2):
+                if len(w1) <= len(w2) and w1 != w2 and (w1 in w2):
                     res.append(w1)
                     break 
         return res
