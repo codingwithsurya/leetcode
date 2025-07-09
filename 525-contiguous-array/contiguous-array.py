863082e06918341d3ef12cd542a81f941ffda522
Surya Subramanian <73920354+codingwithsurya@users.noreply.github.com>
2025-07-08 21:51:37 -0700

    Time: 103 ms (56.51%) | Memory: 23.5 MB (50.87%) - LeetSync

--- 525-contiguous-array/contiguous-array.py
+++ 525-contiguous-array/contiguous-array.py
@@ -1,22 +1,17 @@
 class Solution:
     def findMaxLength(self, nums: List[int]) -> int:
-        diff_to_index = {0:-1} # mapping the diff at index i -> index i
+        diff_to_index = {0: -1}
         diff = 0
-        res = 0 
-
-        for i in range(len(nums)):
-            if nums[i] == 0:
+        res = 0
+        for i, n in enumerate(nums):
+            if n == 0:
                 diff -= 1
             else:
                 diff += 1
             
-            # if we have seen this diff before 
-            # update max length as 
-            # (current_index - corresponding diff index)
             if diff in diff_to_index:
                 res = max(res, i - diff_to_index[diff])
             else:
                 diff_to_index[diff] = i
-            
         return res
-            
+        
